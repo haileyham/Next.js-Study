b4f7c64f651990763c57f19d140af3547bdf3623
hailey ham <roserealslow@gmail.com>
2023-08-12 00:30:45 +0900

    좋아요 버튼 수정(각 작동하도록)

--- market/app/list/page.js
+++ market/app/list/page.js
@@ -4,20 +4,43 @@ import { useState } from "react"
 
 export default function List() {
     let 상품 = ['Melon', 'Watermelon', 'Pineapple']
-    let [수량, set수량] = useState(0)
+    let [수량, set수량] = useState([0, 0, 0])
 
     return (
         <div>
             <h1 className="title">List</h1>
+
+            <span>{수량[0]}</span>
+            <button onClick={() => {
+                let copy = [...수량]; //기존에 있던것 복사
+                copy[0]++ //복사본 플러스
+                set수량(copy);
+
+                //이렇게 하는 이유?
+                // state 변경함수 사용시, 새로운state == 기존state 일 경우 변경 x(기존state랑 같으면 변경x)
+                // 그래서 복사해서 사용
+                // 근데 그냥 수량이 아니라 [...수량] 하는 이유는?
+                // 그냥 let copy = 수량 했을 때는 copy == 수량 비교해보면 같다고 나오기 때문(이유는 array자료 주소값참조이기 때문)에 수정해도 같다고 나옴. 화살표 방향을 복사한것이기 때문
+                // 그래서 [...수량] 이렇게 독립적인 array 만들어서 복사해줌
+            }}>+</button>
+
             {
                 상품.map((a, i) => {
                     return (
                         <div className="food" key={i}>
                             <img src={`/food${i}.png`} alt={a} className="food-img" />
                             <h4>{a} $40</h4>
-                            <span> {수량} </span>
-                            <button onClick={() => { set수량(수량 + 1) }}>+</button>
-                            <button onClick={() => { set수량(수량 - 1) }}>-</button>
+                            <span> {수량[i]} </span>
+                            <button onClick={() => {
+                                let copy = [...수량];
+                                copy[i]++
+                                set수량(copy); //위에서 copy[i]번째를 증가시키고 있고, 그것을 set수량에 새로 변경 / 출력되는건 위쪽의 span에서 수량[i]번째
+                            }}>+</button>
+                            <button onClick={() => {
+                                let copy = [...수량];
+                                copy[i]--
+                                set수량(copy);
+                            }}>-</button>
                         </div>
                     )
                 })
@@ -36,3 +59,6 @@ export default function List() {
 //좋아요 숫자 클릭하면 +1 하도록
 //set수량(수량 + 1) // 수량++ 할경우 버튼 두번씩 눌러줘야함
 //근데 위에처럼 원본에 +1하지말고, ...복사본 만들어서 사용
+
+//똑같이 움직이기 때문에
+//수량 state에 array로 집어넣기 [0,0,0]
