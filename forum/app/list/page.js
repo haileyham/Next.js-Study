aa9c1f61da948fa3fc7f9397feadc2aef7ee76bc
hailey ham <roserealslow@gmail.com>
2023-08-14 16:03:52 +0900

    link 연결

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -1,4 +1,5 @@
 import { connectDB } from '@/util/database';
+import Link from 'next/link';
 import React from 'react'
 
 export default async function List() {
@@ -13,9 +14,12 @@ export default async function List() {
                 result.map((a, i) => { //{return()}없애도됨. 없앨때 중괄호{}랑 return 같이 없애야함}
                     return (
                         <div className="list-item" key={i}>
-                            <h4>{result[i].title}</h4>
-                            <p>{result[i].content}</p>
-                            {/* a.title 혹은 a.content 같기 때문에취향차이 */}
+                            <Link href={`/detail/${result[i]._id}`}>
+                                <h4>{result[i].title}</h4>
+                                <p>{result[i].content}</p>
+                                {/* a.title 혹은 a.content 같기 때문에취향차이 */}
+
+                            </Link>
                         </div>
                     )
                 })
