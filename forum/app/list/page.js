92c6df34e1e4eb7f2cd7e704f56d91a625074034
hailey ham <roserealslow@gmail.com>
2023-08-16 12:36:11 +0900

    삭제기능1

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -2,30 +2,15 @@ import { connectDB } from '@/util/database';
 import Link from 'next/link';
 import React from 'react'
 import DetailLink from './DetailLink';
+import ListItem from './ListItem';
 
 export default async function List() {
     const db = (await connectDB).db("forum");
     let result = await db.collection('post').find().toArray();
-    // console.log(result)
-    // console.log(result[0].title)
 
     return (
         <div className="list-bg">
-            {
-                result.map((a, i) => { //{return()}없애도됨. 없앨때 중괄호{}랑 return 같이 없애야함}
-                    return (
-                        <div className="list-item" key={i}>
-                            <Link href={`/detail/${result[i]._id}`}>
-                                <h4>{result[i].title}</h4>
-                            </Link>
-                            <Link href={`/edit/${a._id}`}>수정🖍</Link>
-                            <p>{result[i].content}</p>
-                            {/* a.title 혹은 a.content 같기 때문에취향차이 */}
-                            {/* <DetailLink /> */}
-                        </div>
-                    )
-                })
-            }
+            <ListItem result={result} />
         </div>
     )
 }
