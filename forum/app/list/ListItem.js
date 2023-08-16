c26b330a0393281d257d93f42d9047efbe382655
hailey ham <roserealslow@gmail.com>
2023-08-16 13:27:42 +0900

    삭제기능구현3

--- forum/app/list/ListItem.js
+++ forum/app/list/ListItem.js
@@ -20,6 +20,18 @@ export default function ListItem({ result }) {
                                 fetch('/api/post/delete', {
                                     method: "DELETE",
                                     body: result[i]._id
+                                }).then((r) => {
+                                    if (r.status == 200) {
+                                        return r.json()
+                                    } else {
+                                        //서버가 에러코드전송시 실행할코드
+                                    }
+                                }).then((result) => {
+                                    //성공시 실행할코드
+                                    console.log(result) //delete.js에서 보낸 json 삭제완료문구뜸
+                                }).catch((error) => {
+                                    //인터넷문제 등으로 실패시 실행할코드
+                                    console.log(error)
                                 })
                             }}> 삭제🗑</span>
                             <p>{result[i].content}</p>
@@ -33,3 +45,7 @@ export default function ListItem({ result }) {
 
 //(참고) DELETE 요청시 데이터안가면 POST 로 바꿔서 하기
 // fetch통해서 DELETE, body에는 props로 받은 DB의 id값 넘겨주고 있음
+
+// [1] Ajax 요청 완료시 코드실행은 fetch().then(()=>{})
+// [2] Ajax 요청 완료시 서버가 보낸 데이터 출력
+// [3] Ajax 에러처리 
--- forum/pages/api/post/delete.js
+++ forum/pages/api/post/delete.js
@@ -3,15 +3,18 @@ import { ObjectId } from 'mongodb';
 import React from 'react'
 
 export default async function handler(요청, 응답) {
-
-    const db = (await connectDB).db("forum");
-    let result = await db.collection('post').deleteOne({ _id: new ObjectId(요청.body) });
-    // 응답.status(200).redirect(302, '/list')
-
+    try {
+        const db = (await connectDB).db("forum");
+        let result = await db.collection('post').deleteOne({ _id: new ObjectId(요청.body) });
+        return 응답.status(200).json('삭제완료') //삭제완료 문구를 보내고있음
+    } catch (error) {
+        return 응답.status(500).json('삭제에러')
+    }
 }
 
 // 서버는 요청받으면 DB글 삭제
 // deleteOne(); document 1개 삭제
 // ListItem에서 body에 id 값 보냈기 때문에 요청.body까지만 씀(id안붙임 바보양ㅁ)
 // connectDB,ObjectID import 까먹지 말기 !
-//
+
+// 삭제후 새로고침
