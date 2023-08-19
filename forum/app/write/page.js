0381fcc6a509849ed96e5f7edd9ba52e0ffea682
hailey ham <roserealslow@gmail.com>
2023-08-19 12:08:02 +0900

    로그인했을때만 write페이지 접속하도록

--- forum/app/write/page.js
+++ forum/app/write/page.js
@@ -1,15 +1,25 @@
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
+import { getServerSession } from 'next-auth'
 import React from 'react'
 
-export default function Write() {
-    return (
-        <div className='p-20'>
-            <h1>글작성</h1>
-            <form action="/api/post/new" method="POST">
-                {/* 서버/api/post/new로 보내짐 > 유저가 /api/post/new로 요청시 실행됨 */}
-                <input type="text" name="title" placeholder="글 제목" />
-                <input type="text" name="content" placeholder="글 내용" />
-                <button type="submit">등록</button>
-            </form>
-        </div>
-    )
+export default async function Write() {
+    let session = await getServerSession(authOptions)
+    // console.log(session)
+
+    // 로그인 했을 때만 write 페이지 접속 가능하도록
+    if (session == null) {
+        return <div>로그인하세용</div>
+    } else {
+        return (
+            <div className='p-20'>
+                <h1>글작성</h1>
+                <form action="/api/post/new" method="POST">
+                    {/* 서버/api/post/new로 보내짐 > 유저가 /api/post/new로 요청시 실행됨 */}
+                    <input type="text" name="title" placeholder="글 제목" />
+                    <input type="text" name="content" placeholder="글 내용" />
+                    <button type="submit">등록</button>
+                </form>
+            </div>
+        )
+    }
 }
