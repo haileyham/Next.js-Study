66bb250d7529b57c3ffa31a54f7fab86351f61fc
hailey ham <roserealslow@gmail.com>
2023-08-21 13:38:13 +0900

    삭제요청에 role admin 관리자일 경우 모두 가능하도록 처리

--- forum/pages/api/post/delete.js
+++ forum/pages/api/post/delete.js
@@ -9,11 +9,15 @@ export default async function handler(요청, 응답) {
         let session = await getServerSession(요청, 응답, authOptions)
         const db = (await connectDB).db("forum");
         let 찾자 = await db.collection("post").findOne({ _id: new ObjectId(요청.body) })
+        // console.log(session)
 
         if (session) {
             if (찾자.author === session.user.email) {
                 let result = await db.collection('post').deleteOne({ _id: new ObjectId(요청.body) });
                 return 응답.status(200).json('삭제완료') //삭제완료 문구를 보내고있음
+            } if (session.user.role === "admin") { //관리자 계정일 경우 모든 글 삭제 가능하도록
+                let result = await db.collection('post').deleteOne({ _id: new ObjectId(요청.body) });
+                return 응답.status(200).json('삭제완료')
             } else {
                 return 응답.status(500).json({ error: '현재유저와 작성자 불일치' }) //error메시지 맞게 띄우려고 error에 담음 > 클라이언트에서는 errorData.error로 출력
             }
