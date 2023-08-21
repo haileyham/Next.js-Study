4c5552a323eb33a951f9476dc60fa329b3748b37
hailey ham <roserealslow@gmail.com>
2023-08-21 13:37:34 +0900

    role 추가 normal,admin

--- forum/pages/api/auth/signup.js
+++ forum/pages/api/auth/signup.js
@@ -23,6 +23,7 @@ export default async function handler(요청, 응답) {
             // console.log(요청.body); //비번 그냥 출력됨. 집어넣어주기
             요청.body.password = hash; //[3] pw부분 암호화한걸로 변경해주기
             // let db = (await connectDB).db('forum');
+            요청.body.role = "normal"; // 기본유저 normal 부여 / 관리자는 admin으로
             await db.collection('user_cred').insertOne(요청.body); //DB로 전송 user_cred 콜렉션에 회원정보 보관 / 유저가 보낸 요청.body를 넣어줌{name:abc,email:abc,password:abc}들어감 / [1]비번은 암호화 처리 필요
             // 비번 암호화해서 저장 : 라이브러리 npm install bcrypt 설치
             응답.status(200).json('요청성공'); //DB forum의 user_cred에 저장됨
