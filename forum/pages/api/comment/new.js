21fab08d23e46c2aae322423c30e6410c71fadd7
hailey ham <roserealslow@gmail.com>
2023-08-21 15:24:12 +0900

    댓글 서버 구현

---
+++ forum/pages/api/comment/new.js
@@ -0,0 +1,37 @@
+import { connectDB } from '@/util/database'
+import { getServerSession } from 'next-auth'
+import React from 'react'
+import { authOptions } from '../auth/[...nextauth]'
+import { ObjectId } from 'mongodb'
+
+// export default async function handler(요청, 응답) {
+//     if (요청.method == "POST") {
+//         let 저장내용 = {
+//             content: '댓글내용',
+//             parent: '부모게시물_id',
+//             author: '유저이메일'
+//         }
+//     }
+//     console.log(요청.body)
+//     const db = (await connectDB).db("forum");
+//     let result = await db.collection('comment').insertOne(저장내용);
+//     return 응답.status(200).json('저장완료')
+// }
+
+
+
+export default async function handler(요청, 응답) {
+    let session = await getServerSession(요청, 응답, authOptions)
+    console.log(요청.body)
+    if (요청.method == "POST") {
+        let 저장내용 = {
+            content: 요청.body.comment, //댓글 내용
+            parent: new ObjectId(요청.body._id), //해당글id
+            author: session.user.email //댓글 작성자
+        }
+        console.log(요청.body)
+        const db = (await connectDB).db("forum");
+        let result = await db.collection('comment').insertOne(저장내용);//comment에 따로 저장(댓글내용,해당글id,댓글작성자)
+        return 응답.status(200).json('저장완료')
+    }
+}
