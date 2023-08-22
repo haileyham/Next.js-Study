7f2e73fffc72255e70ce90970a6ea59a1a63b8a6
hailey ham <roserealslow@gmail.com>
2023-08-22 16:38:43 +0900

    댓글작성자 일치하면 삭제가능하도록

--- forum/app/detail/[id]/Comment.js
+++ forum/app/detail/[id]/Comment.js
@@ -56,21 +56,6 @@ export default function Comment({ _id }) {
         }
     };
 
-    // const handleDelete = () => {
-    //     fetch(`/api/comment/delete?id=${a.}`, {
-    //         method: 'DELETE'
-    //     })
-    //         .then((response) => response.json())
-    //         .then((result) => {
-    //             console.log(result); // '삭제완료' 메시지 출력
-    //             // 서버에서 삭제 작업이 완료되었다면, 적절한 업데이트 작업을 수행하거나 상태를 업데이트하세요.
-    //         })
-    //         .catch((error) => {
-    //             console.error('Error deleting comment:', error);
-    //             // 에러 처리 로직을 추가하십시오.
-    //         });
-    // };
-
     return (
         <div>
             {/* 수평줄 생성 hr */}
@@ -85,7 +70,7 @@ export default function Comment({ _id }) {
                                     <span>작성자 : {a.author_name} / </span>
                                     댓글 : {a.content}
                                     <button onClick={() => {
-                                        fetch(`/api/comment/delete?id=${a._id}`, {//댓글 id를 서버로 보내줘야함
+                                        fetch(`/api/comment/delete?id=${a._id}&author=${a.author}`, {//댓글 id를 서버로 보내줘야함/ &붙여서 데이터 추가 전송하기 / 댓글작성자 구분위해서 author 전송
                                             method: 'DELETE'
                                         })
                                             .then((response) => response.json())
--- forum/pages/api/comment/delete.js
+++ forum/pages/api/comment/delete.js
@@ -5,8 +5,21 @@ import { connectDB } from '@/util/database';
 import { ObjectId } from 'mongodb';
 
 export default async function handler(요청, 응답) {
-    let session = await getServerSession(요청, 응답, authOptions);
-    const db = (await connectDB).db('forum');
-    let result = await db.collection('comment').deleteOne({ _id: new ObjectId(요청.query.id) })
-    return 응답.status(200).json('삭제완료');
+    if (요청.method == "DELETE") {
+        let session = await getServerSession(요청, 응답, authOptions);
+        const db = (await connectDB).db('forum');
+        // console.log(요청.query.author)
+        // console.log(요청.query.id)
+        let 찾자 = await db.collection('comment').findOne({ _id: new ObjectId(요청.query.id) }) //작성자 찾기위해서 해당 댓글을 미리 찾고!!! (헷갈리지말기, 댓글자체id로 찾고 있기 때문에 id 집어넣어서 일단 해당댓글 정보가져와서 밑에 if문에서 찾자.author로 비교하기)
+        // console.log(찾자)
+
+        if (session) {//로그인 유무 체크
+            if (찾자.author === session.user.email) {//댓글 작성자랑 삭제요청유저 같으면 삭제하도록
+                let result = await db.collection('comment').deleteOne({ _id: new ObjectId(요청.query.id) })//댓글 id 받음
+                return 응답.status(200).json('삭제완료');
+            } else {
+                return 응답.status(500).json('현재유저와 댓글 작성자 불일치')
+            }
+        }
+    }
 }
