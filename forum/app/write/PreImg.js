94127cdc5dbb5584131e5f8b73bdf997b520a750
hailey ham <roserealslow@gmail.com>
2023-08-24 12:26:38 +0900

    이미지 선택 주석추가

--- forum/app/write/PreImg.js
+++ forum/app/write/PreImg.js
@@ -6,6 +6,10 @@ export default function PreImg() {
     const [preImg, setPreImg] = useState('')
     const handleFileChange = ((e) => {
         const file = e.target.files[0];
+        // console.log(file)
+        // console.log(file.name) //file저장이름 보여줌
+        // console.log(encodeURIComponent(file.name)) //file한글명일경우 인코딩해줌 / 서버주고받을때 filename 변수에 담아서 사용해서 보내보기
+
         setPreImg(URL.createObjectURL(file));
     })
 
