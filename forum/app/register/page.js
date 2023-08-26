b4fe2ce212ad520583c99d8469ac2aead510aab6
hailey ham <roserealslow@gmail.com>
2023-08-26 21:45:05 +0900

    회원가입 페이지 임시 수정

--- forum/app/register/page.js
+++ forum/app/register/page.js
@@ -3,11 +3,12 @@ import React from 'react'
 export default function Register() {
     return (
         <div>
-            <form action="/api/auth/signup" method="POST">
+            <form action="/api/auth/signup" method="POST" className='registerBox'>
+                <h1>회원가입</h1>
                 <input name="name" type="text" placeholder="name" />
                 <input name="email" type="text" placeholder="email" />
                 <input name="password" type="password" placeholder="pw" />
-                <button type="submit">id/pw 가입요청</button>
+                <button type="submit">가입요청</button>
             </form>
         </div>
     )
